d844488417e0951f253c037efe4924a49be14c61
Anant Sharan Pandey <infiniteasp8>
2023-03-31 18:04:55 +0530

    Initial

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import Sidebar from './components/Sidebar'
+
+function App() {
+  return (
+    <div className="main-rms">
+      <Sidebar />
+      <div className="container-rms">
+        <h1 className="title-rms">RMS</h1>
+        <p className="info">Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, doloribus.</p>
+        
+      </div>
+    </div>
+  )
+}
+
+export default App
